d0a9917d4cb5531a8102992c45373d26bd67e844
Alanko Jarno N <analanko@turso01.cs.helsinki.fi>
2026-03-13 13:05:42 +0200

    Kraken commands

--- chromosome_benchmark/scripts/print_kraken_query_benchmark_commands.py
+++
@@ -1,3 +0,0 @@
-for t in [64,32,16,8,4,2,1]:
-    for k in [15,31,47,63]:
-        print(f"/usr/bin/time -v kraken2 --db kraken_k{k} --gzip-compressed --output out/hg002-k{k}-t{t}-kraken.txt --threads {t} query/hg002v1.1.fasta.gz &> logs/kraken-query-k{k}-t{t}.log")
