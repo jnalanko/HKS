9762b602edcb8e9244758d5f03d675f17d46ea62
Alanko Jarno N <analanko@turso01.cs.helsinki.fi>
2026-03-13 18:34:15 +0200

    Add heatmap data

--- chromosome_benchmark/scripts/print_kraken_to_heatmaps_commands.py
+++ chromosome_benchmark/scripts/print_kraken_to_heatmaps_commands.py
@@ -1,7 +1,9 @@
 print("set -xue")
 
 for k in [15,31,47,63]:
-    t = 16
-    print(f"rust-script scripts/kraken_to_bed out/hg002-k{k}-t{t}-kraken.txt out/hg002-k{k}-kraken.bed")
-    print(f"rust-script scripts/compute_heatmap_data.rs out/hg002-k{k}-kraken.bed out/heatmap-k{k}-kraken.tsv")
-    print(f"python3 scripts/draw_heatmap.py out/heatmap-k{k}-kraken.tsv plots/heatmap-k{k}-kraken.png --kraken")
+    for m in [15,22,32]:
+        if m > k: continue
+        t = 32
+        print(f"rust-script scripts/kraken_to_bed out/hg002-k{k}-m{m}-t{t}-kraken.txt out/hg002-k{k}-m{m}-kraken.bed")
+        print(f"rust-script scripts/compute_heatmap_data.rs out/hg002-k{k}-m{m}-kraken.bed out/heatmap-k{k}-m{m}-kraken.tsv")
+        print(f"python3 scripts/draw_heatmap.py out/heatmap-k{k}-m{m}-kraken.tsv plots/heatmap-k{k}-m{m}-kraken.png --kraken")
