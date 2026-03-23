eb3c40ae0e6cd0889a0d2df80b5517c998c6fe02
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-03-23 23:00:53 +0200

    Update benchmark build command

--- chromosome_benchmark/scripts/print_hks_build_benchmark_commands.py
+++ chromosome_benchmark/scripts/print_hks_build_benchmark_commands.py
@@ -1,7 +1,7 @@
 s = 63
 
 for t in [1,2,4,8,16,32,64]:
-    print(f"/usr/bin/time -v hks build -s 63 -o index/CHM13-s63.hks -t {t} --external-memory temp --label-by-file feature_tree/chromosome_fof.txt --labels feature_tree/chromosome_names.txt --hierarchy feature_tree/chromosome_tree.txt -b CHM13/sbwt/CHM13-k63-added-dummies.sbwt -l CHM13/sbwt/CHM13-k63-added-dummies.lcs &> logs/hks-build-s{s}-t{t}.log")
+    print(f"/usr/bin/time -v hks build -s 63 -o index/CHM13-s63.hks -t {t} --external-memory temp --label-by-file feature_tree/chromosome_fof.txt --labels feature_tree/chromosome_names.txt --hierarchy feature_tree/chromosome_tree.txt --load-sbwt CHM13/sbwt/CHM13-k63-added-dummies.sbwt --load-lcs CHM13/sbwt/CHM13-k63-added-dummies.lcs &> logs/hks-build-s{s}-t{t}.log")
 
 
 #--label-by-file feature_tree/chromosome_fof.txt --labels feature_tree/chromosome_names.txt
